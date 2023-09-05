8e40b8cd87b1e1c0927ed773565c9db5d71c8fa4
Ciprian Serac <farazahar14@gmail.com>
2023-09-05 07:04:34 -0500

    Added the booking form and some tests

---
+++ src/components/Confirmation.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Confirmation = () => {
+  return <p>Your booking has been confirmed!</p>;
+};
--- src/components/pages/BookingPage.js
+++ src/components/pages/BookingPage.js
@@ -1,45 +1,19 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import '../../css/Booking.css';
+import { BookingForm } from './../BookingForm';
 
 export const BookingPage = () => {
   const [date, setDate] = useState(new Date());
 
   return (
-    <form className='submit-form'>
-      <label htmlFor='res-date'>Choose date</label>
-      <input
-        type='date'
-        id='res-date'
-        value={date.toISOString().split('T')[0]}
-        required
-        min={new Date().toISOString().split('T')[0]}
-      />
-
-      <label htmlFor='res-time'>Choose time</label>
-      <select id='res-time' required>
-        <option value='1'>1</option>
-        <option value='2'>2</option>
-        <option value='3'>3</option>
-      </select>
-
-      <label htmlFor='guests'>Number of guests</label>
-      <input
-        type='number'
-        placeholder='1'
-        min='1'
-        max='10'
-        id='guests'
-        required
-      />
-
-      <label htmlFor='occasion'>Occasion</label>
-      <select id='occasion' value='Which' required>
-        <option value='birthday'>Birthday</option>
-        <option value='anniversary'>Anniversary</option>
-        <option value='other'>Other</option>
-      </select>
-
-      <button type='submit'>Submit</button>
-    </form>
+    <div className='reservation-w50 fh'>
+      <div>
+        <h2 className='welcome-header'>
+          Welcome to Little Lemon! Book a table:
+        </h2>
+        <BookingForm />
+      </div>
+    </div>
   );
 };
